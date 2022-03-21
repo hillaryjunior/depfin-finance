fe11d2ebee10a534ab65001a276bf9bbbc464dcd
Teddy Kisala <teddy@vetro.co.za>
2022-03-21 22:40:55 +0200

    minor fixes

--- utils/Fab.js
+++ utils/Fab.js
@@ -4,14 +4,14 @@ import WhatsAppIcon from '@mui/icons-material/WhatsApp';
 
 function Fab() {
   return (
-          <div className="float">
-                  <Link href= "/" >
-                          <a className="my-float"> 
-                                  < WhatsAppIcon  />
-                          </a>
-                  </Link>
+    <div className="float">
+      <Link href="https://api.whatsapp.com/send?phone=+27630900202">
+        <a className="my-float">
+          <WhatsAppIcon />
+        </a>
+      </Link>
     </div>
-  )
+  );
 }
 
 export default Fab

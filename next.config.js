7dca0b1a078222c00a7217589a394ee9b830bec7
Teddy <devted19@gmail.com>
2023-07-10 10:22:27 +0200

    pushed lastest changes and updated read me file

--- next.config.js
+++ next.config.js
@@ -1,35 +1,38 @@
 const headers = require('./security')
 module.exports = {
   reactStrictMode: true,
-  trailingSlash: true,
+  trailingSlash: false,
  
 
   // create exportPathMap
 
-  exportPathMap: async function (
-    defaultPathMap,
-    { dev, dir, outDir, distDir, buildId }
-  ) {
-    return {
-      "/": { page: "/" },
-      "/about": { page: "/about" },
-      "/personal/business/loan/apply": {
-        page: "/personal/business/loan/apply",
-      },
-      "/complaints": { page: "/complaints" },
-      "/contact": { page: "/contact" },
-      "/faqs": { page: "/faqs" },
-      "/privacypolicy": { page: "/privacypolicy" },
-      "/sitemap": { page: "/sitemap" },
+  // exportPathMap: async function (
+  //   defaultPathMap,
+  //   { dev, dir, outDir, distDir, buildId }
+  // ) {
+  //   return {
+  //     "/": { page: "/" },
+  //     "/about": { page: "/about" },
+  //     "/personal/business/loan/apply": {
+  //       page: "/personal/business/loan/apply",
+  //     },
+  //     "/apply": {
+  //       page: "/personal/business/loan/apply",
+  //     },
+  //     "/complaints": { page: "/complaints" },
+  //     "/contact": { page: "/contact" },
+  //     "/faqs": { page: "/faqs" },
+  //     "/privacypolicy": { page: "/privacypolicy" },
+  //     "/sitemap": { page: "/sitemap" },
 
-      "/solutions": { page: "/solutions" },
-      "/terms": { page: "/terms" },
-      "/conflictofinterest": { page: "/conflictofinterest" },
-      "/auth/login": { page: "/auth/login" },
-      "/auth/reset": { page: "/auth/reset" },
-      "/auth/profile": { page: "/auth/profile" },
-    };
-  },
+  //     "/solutions": { page: "/solutions" },
+  //     "/terms": { page: "/terms" },
+  //     "/conflictofinterest": { page: "/conflictofinterest" },
+  //     "/auth/login": { page: "/auth/login" },
+  //     "/auth/reset": { page: "/auth/reset" },
+  //     "/auth/profile": { page: "/auth/profile" },
+  //   };
+  // },
 
   images: {
     loader: "akamai",

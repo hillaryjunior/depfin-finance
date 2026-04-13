add7b437525a548917240e41d3623f435c187d75
localseomanagementama-creator <localseomanagementama@gmail.com>
2026-04-13 18:10:18 +0530

    Update apply.js

--- pages/personal/business/loan/apply.js
+++ pages/personal/business/loan/apply.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import link from 'next/link'
 import Head from 'next/head'
+<Head>
+  <title>Apply Online Loan South Africa | Instant Approval & Same Day Payout</title>
+  <meta name="description" content="Apply for instant loans online in South Africa. Fast approval, same day payout loans, unsecured loans and loans for blacklisted or bad credit clients." />
+</Head>
 import ApplyLayout from '../../../../Layouts/ApplyLayout';
 
 function Apply() {

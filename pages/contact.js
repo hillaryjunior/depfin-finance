012676be9b4fbba3207c0e64ee3d16cc43dcc208
localseomanagementama-creator <localseomanagementama@gmail.com>
2026-04-13 17:46:33 +0530

    Update contact.js

--- pages/contact.js
+++ pages/contact.js
@@ -1,4 +1,8 @@
 import Head from 'next/head'
+<Head>
+  <title>Contact Depfin Finance | Loan Help South Africa</title>
+  <meta name="description" content="Contact Depfin Finance for quick loans, business funding and personal loans in South Africa. Get expert help with applications and approvals." />
+</Head>
 import ContactLayout from '../Layouts/ContactLayout'
 import  '../sass/components/Contact.module.scss'
 function contact() {
